9230dc0e1cf21acfa0b563500ecb6240f2168673
EagleHawk <51789834+EagleHawk2244@users.noreply.github.com>
2020-07-27 10:27:29 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,34 @@
-import 
+import turtle
+import time
+t = turtle.Pen()
+
+branch = []
+code = "1[0]0" # Change this to modify the structure of the tree
+
+t.left(90)
+t.up()
+t.backward(300)
+t.down()
+t.speed(10)
+
+
+for c in code:
+    if c == "1":
+        t.pencolor("brown")
+        t.forward(100) # Change this to modify the size of the tree
+
+    elif c == "0":
+        t.pencolor("green")
+        t.forward(3) # Change this to modify the size of the leaves
+
+    elif c == "[":
+        t.left(45)
+        branch.append((t.position(), t.heading()))
+
+    elif c == "]":
+        t.up()
+        t.goto(branch[len(branch) -1][0], None)
+        t.setheading(branch[len(branch) -1][1])
+        t.right(90)
+        t.down()
+        del branch[len(branch) -1]
